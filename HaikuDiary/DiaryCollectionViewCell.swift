d94d027e14461703156609d4a6bcd486f3096afc
HisadaYuhei <hsdy20010918@gmail.com>
2024-09-16 17:25:14 +0900

    3

---
+++ HaikuDiary/DiaryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryCollectionViewCell.swift
+//  HaikuDiary
+//
+//  Created by 久田　悠平 on 2024/09/16.
+//
+
+import UIKit
+
+class DiaryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ HaikuDiary/ReadDiaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReadDiaryViewController.swift
+//  HaikuDiary
+//
+//  Created by 久田　悠平 on 2024/09/16.
+//
+
+import UIKit
+
+class ReadDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
